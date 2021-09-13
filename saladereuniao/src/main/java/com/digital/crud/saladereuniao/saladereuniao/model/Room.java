3ada87dd9c4ed3be3ccfb306fd83f07d3884eb94
patrickfinzi <patrickfinzivieira@gmail.com>
2021-09-13 05:44:36 -0300

    sala reuniao java angular

---
+++ saladereuniao/src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,35 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "meetingroom")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "data", nullable = false)
+    private String data;
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+    @Override
+    public String toString(){
+        return "Room [id="+id+",name="+name+",startHour="+startHour+",endHour="+endHour+"]";
+    }
+}
